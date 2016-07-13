61ad50bc31f256c1275e5aff08025c17a4bf2a31
sue445 <sue445@sue445.net>
2016-07-13 14:21:27 +0900

    bundle gem capistrano-itamae --mit

---
+++ lib/capistrano/itamae.rb
@@ -0,0 +1,7 @@
+require "capistrano/itamae/version"
+
+module Capistrano
+  module Itamae
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/itamae/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Itamae
+    VERSION = "0.1.0"
+  end
+end
