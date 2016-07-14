3ad8c0af69aed7474a2827d07c680271bfe26a65
sue445 <sue445@sue445.net>
2016-07-15 01:59:29 +0900

    Fix. can not ssh to remote

--- lib/capistrano/itamae/dsl.rb
+++ lib/capistrano/itamae/dsl.rb
@@ -17,35 +17,38 @@ module Capistrano
             options
           end
 
+        # NOTE: store server (`host` is changed to localhost in `run_locally`)
+        server = host
+
         run_locally do
           Bundler.with_clean_env do
-            execute *generate_itamae_ssh_command(recipe_paths, itamae_options)
+            execute *generate_itamae_ssh_command(server, recipe_paths, itamae_options)
           end
         end
       end
 
       private
 
-      def generate_itamae_ssh_command(recipe_paths, itamae_options)
+      def generate_itamae_ssh_command(server, recipe_paths, itamae_options)
         command = [:bundle, :exec, itamae_bin_name, :ssh]
 
         command += recipe_paths
         command += itamae_options unless itamae_options.empty?
-        command += ssh_options
+        command += ssh_options(server)
 
         command
       end
 
-      def ssh_options
+      def ssh_options(server)
         ssh_options = fetch(:ssh_options, {}).dup
-        ssh_options.merge!(host.ssh_options) if host.ssh_options
-        ssh_options[:user] ||= host.user
-        ssh_options[:port] ||= host.port
-        ssh_options[:keys] ||= host.keys
+        ssh_options.merge!(server.ssh_options) if server.ssh_options
+        ssh_options[:user] ||= server.user
+        ssh_options[:port] ||= server.port
+        ssh_options[:keys] ||= server.keys
         ssh_options[:key]  ||= ssh_options[:keys].first if ssh_options[:keys] && !ssh_options[:keys].empty?
 
         options = []
-        options << "--host #{host.hostname}"
+        options << "--host #{server.hostname}"
         options << "--user #{ssh_options[:user]}" if ssh_options[:user]
         options << "--port #{ssh_options[:port]}" if ssh_options[:port]
         options << "--key #{ssh_options[:key]}" if ssh_options[:key]
