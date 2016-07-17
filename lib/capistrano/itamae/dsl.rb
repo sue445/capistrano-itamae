c1be751b0e899088d6a8d56ce5e3c862c5fdf70f
sue445 <sue445@sue445.net>
2016-07-17 14:28:41 +0900

    Use directory Itamae::Runner instead of bundle exec itamae ssh

--- lib/capistrano/itamae/dsl.rb
+++ lib/capistrano/itamae/dsl.rb
@@ -1,12 +1,10 @@
 module Capistrano
   module Itamae
     module DSL
-      require "bundler"
-
       # Run `itamae ssh`
       # @param recipe_files [String, Array<String>]
-      # @param options [Array] itamae ssh options (default is itamae_ssh_default_options)
-      def itamae_ssh(recipe_files, *options)
+      # @param options [Hash<Symbol, String>] itamae ssh options (default is itamae_ssh_default_options)
+      def itamae_ssh(recipe_files, options = {})
         recipe_files = Array(recipe_files) unless recipe_files.is_a?(Array)
         recipe_paths = recipe_files.map { |file| itamae_cookbooks_path.join(file) }
 
@@ -17,26 +15,15 @@ module Capistrano
             options
           end
 
-        # NOTE: store server (`host` is changed to localhost in `run_locally`)
-        server = host
-
-        run_locally do
-          Bundler.with_clean_env do
-            execute *generate_itamae_ssh_command(server, recipe_paths, itamae_options)
-          end
-        end
+        ::Itamae::Runner.run(recipe_paths, :ssh, itamae_ssh_options(host, itamae_options))
       end
 
       private
 
-      def generate_itamae_ssh_command(server, recipe_paths, itamae_options)
-        command = [:bundle, :exec, itamae_bin_name, :ssh]
-
-        command += recipe_paths
-        command += itamae_options unless itamae_options.empty?
-        command += ssh_options(server)
-
-        command
+      def itamae_ssh_options(server, itamae_options)
+        options = ssh_options(server)
+        options.merge!(itamae_options) unless itamae_options.empty?
+        options
       end
 
       def ssh_options(server)
@@ -47,11 +34,10 @@ module Capistrano
         ssh_options[:keys] ||= server.keys
         ssh_options[:key]  ||= ssh_options[:keys].first if ssh_options[:keys] && !ssh_options[:keys].empty?
 
-        options = []
-        options << "--host #{server.hostname}"
-        options << "--user #{ssh_options[:user]}" if ssh_options[:user]
-        options << "--port #{ssh_options[:port]}" if ssh_options[:port]
-        options << "--key #{ssh_options[:key]}" if ssh_options[:key]
+        options = { host: server.hostname }
+        options[:user] = ssh_options[:user] if ssh_options[:user]
+        options[:port] = ssh_options[:port] if ssh_options[:port]
+        options[:key]  = ssh_options[:key]  if ssh_options[:key]
         options
       end
     end
