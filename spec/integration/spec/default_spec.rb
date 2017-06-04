9d11b11d1687ddf2e6a466e0ab2270ccadaad90c
tkeo <takeofujita@gmail.com>
2017-06-05 00:30:20 +0900

    Use another package because wget is installed in the CI image

--- spec/integration/spec/default_spec.rb
+++ spec/integration/spec/default_spec.rb
@@ -1,7 +1,7 @@
-describe package("wget") do
+describe package("zsh") do
   it { should be_installed }
 end
 
-describe package("wget"), dry_run: true do
+describe package("zsh"), dry_run: true do
   it { should_not be_installed }
 end
